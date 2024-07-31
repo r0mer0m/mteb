7f2dda923da1aec0ac1439c85fe0faf6695b7bda
Miguel Romero <miguelrc@amazon.com>
2024-07-31 02:44:17 -0400

    debug

--- mteb/evaluation/evaluators/__init__.py
+++ mteb/evaluation/evaluators/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from .bitext_mining_evaluator import *
+# from .bitext_mining_evaluator import *
 from .classification_evaluator import *
 from .clustering_evaluator import *
 from .pair_classification_evaluator import *
